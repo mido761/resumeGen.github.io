9b90fced326ec956266580043f4f8e37fd441b47
mido761 <65832996+mido761@users.noreply.github.com>
2024-10-08 11:08:13 +0300

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,13 +1,32 @@
-let hamIcon = document.querySelector('.navbar-toggler-btn');
-var menuList = document.querySelector('.menuList');
-var navTogglerBtn = document.querySelector('.navbar-toggler-btn');
+import { HfInference } from '@huggingface/inference';
+import dotenv from 'dotenv';
+dotenv.config()
+const inference = new HfInference(process.env.HUGGING_FACE_API_KEY);
+let output = ""
 
+let result = ""
 
+let input = ""
+export const fetchInference = async function (inputElem ,outputElem, AssistantMessage) {
+    try {
+        outputElem = ''; // Clear previous 
+        input = inputElem + "\n" + AssistantMessage 
+        console.log("the user message is: " + input)
+        for await (const chunk of inference.chatCompletionStream({
+            model: "tiiuae/falcon-7b-instruct",
+            messages: [{ role: "user", content: input}],
+            max_tokens: 95,
+        })) {
+            output = chunk.choices[0]?.delta?.content || "";
+            outputElem += output.replace("User", "").replace(" summary", "").replace(":", "").replace(/[\r\n]+/g, '');
+            
+        }
+        return outputElem
 
-hamIcon.addEventListener('click', () => {
-    menuList.classList.toggle("active");
-    navTogglerBtn.classList.toggle('active')
-
-})
+    } catch (error) {
+        console.error("Error during inference:", error);
+        outputElem = "Error occurred during inference.";
+    }
+}
 
 
